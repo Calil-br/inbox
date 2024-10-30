b52de6359aa676153c3ba050a9e8d153fd65bf03
lucas calil <lucascalill@icloud.com>
2024-10-29 22:23:38 -0300

    Ajusta scripts e configurações para deploy no Render

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+    strictPort: true
+  }
 });
